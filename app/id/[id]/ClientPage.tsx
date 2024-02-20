a5af49ceffc2f3a47f83e0312faeabcbf8280038
Gregor Weber <mail@dflate.io>
2024-02-20 17:09:24 +0100

    fix resizing

--- app/id/[id]/ClientPage.tsx
+++ app/id/[id]/ClientPage.tsx
@@ -45,7 +45,7 @@ export function ClientPollPage({
   useEffect(() => {
     const el = ref.current;
     if (isFramed && el && poll) {
-      postResizeChanges(el);
+      return postResizeChanges(el);
     }
   }, [isFramed, poll]);
 
